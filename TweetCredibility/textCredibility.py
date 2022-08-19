3bc7f6cace4fbe435ba53e3ebcc2083511ea7c92
francesco cafiero <f.cafiero1@studenti.unisa.it>
2022-08-19 12:07:06 +0200

    ADD bad words part for test credibility of a tweet

---
+++ TweetCredibility/textCredibility.py
@@ -0,0 +1,38 @@
+import json
+import os
+from nltk.tokenize import word_tokenize
+import string
+
+def getText(tweetPath):
+    file = open(tweetPath)
+    data = json.load(file)
+    text = data['text']
+    file.close()
+    return text
+
+def isSpam(testo):
+    pass
+
+def badWords(testo):
+    location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    with open(os.path.join(location, 'bad_words.txt'), 'r') as badDict:
+        profanities= badDict.readlines()
+    testo=testo.translate(str.maketrans('', '', string.punctuation))
+    testo=testo.lower()
+    badCount=0
+    for line in profanities:
+        if line.split('\n')[0] in testo:
+            badCount+=1
+    
+    return round((badCount/len(testo.split()))*100, 2)
+
+def miSpelling(testo):
+    pass
+
+def textCredibility(tweetPath):
+    testo = getText(tweetPath)
+    return isSpam(testo)+badWords(testo)+miSpelling(testo)
+
+
+stringa="my sweet, jesus fuck i: love this planet because there are all those dicks"
+print(badWords(stringa))
