4f0ec53e299fa7e998239c655fa3512b3f4c09a2
francesco cafiero <f.cafiero1@studenti.unisa.it>
2022-08-20 11:40:54 +0200

    RENAME csv file and ADD bag of word function and evaluation

--- TweetCredibility/spam_detector.py
+++ TweetCredibility/spam_detector.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import string
 import re
+import os
 
 def cleanText(testo):
     testo=re.sub("http\S+", '',  testo)
@@ -16,16 +17,53 @@ def predict_text(testo, spam_bow, non_spam_bow, FRAC_SPAM_TEXT):
     spam_score = sum([np.log(probability) for probability in spam_prob]) + np.log(FRAC_SPAM_TEXT)
     non_spam_score = sum([np.log(probability) for probability in non_spam_prob]) + np.log(1-FRAC_SPAM_TEXT)
 
-    print('Spam Score: ', spam_score)
-    print('Non-Spam Score: ',non_spam_score)
+    #print('Spam Score: ', spam_score)
+    #print('Non-Spam Score: ',non_spam_score)
 
     return spam_score >= non_spam_score
 
-def prepare_model(csv_path):
-    spam_df = pd.read_csv(csv_path)
-    spam_df = spam_df[['SPAM', 'TEXT']]
-    spam_df.SPAM = spam_df.SPAM.apply(lambda s: True if s=='spam' else False)
+def prepare_dataframe(csv_path):
+    spam_df = pd.read_csv(csv_path, delimiter='\t', header=None)
+
+    spam_df = spam_df.rename(columns={0:'SPAM', 1:'TEXT'})
+    
+
+    spam_df.SPAM = spam_df.SPAM.apply(lambda s: True if s=='spam,' else False)
 
     spam_df.TEXT = spam_df.TEXT.apply(lambda text: cleanText(text))
 
-    spam_df = spam_df.sample(frac=1, random_state=42)
+    spam_df = spam_df.sample(frac=1, random_state=28)
+
+    train_spam_df=spam_df.iloc[:int(len(spam_df)*0.8)]
+    val_spam_df=spam_df.iloc[int(len(spam_df)*0.8):]
+
+    FRAC_SPAM_TEXTS = train_spam_df.SPAM.mean()
+
+    return train_spam_df, val_spam_df, FRAC_SPAM_TEXTS
+
+def create_bag_of_words(train_spam_df):
+    spam_words = ' '.join(train_spam_df[train_spam_df.SPAM == True].TEXT).split()
+    non_spam_words = ' '.join(train_spam_df[train_spam_df.SPAM == False].TEXT).split()
+    common_words = set(spam_words).intersection(set(non_spam_words))
+
+    spam_bow = dict()
+    non_spam_bow = dict()
+
+    for word in common_words:
+        spam_bow[word] = spam_words.count(word)/len(spam_words)
+        non_spam_bow[word] = non_spam_words.count(word)/len(non_spam_words)
+    
+    return spam_bow, non_spam_bow
+
+
+
+file_path=os.path.realpath(os.path.dirname(__file__))+'/train_datasetspam.csv'
+train_spam_df, val_spam_df, FRAC_SPAM_TEXT=prepare_dataframe(file_path)
+spam_bow, non_spam_bow = create_bag_of_words(train_spam_df)
+
+predictions = val_spam_df.TEXT.apply(lambda t: predict_text(t.split(), spam_bow, non_spam_bow, FRAC_SPAM_TEXT))
+frac_spam_messages_correctly_detected = np.sum((predictions == True) & (val_spam_df.SPAM == True)) / np.sum(val_spam_df.SPAM == True)
+print('Fraction Spam Correctly Detected: %s'%frac_spam_messages_correctly_detected)
+
+
+
