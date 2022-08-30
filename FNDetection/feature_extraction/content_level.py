bf39244213c079a292ecbbce8a533f73d9816854
Cosomazio <73337360+Cosomazio@users.noreply.github.com>
2022-08-30 11:19:54 +0200

    adding new function for feature extraction in content_level

--- FNDetection/feature_extraction/content_level.py
+++ FNDetection/feature_extraction/content_level.py
@@ -2,6 +2,7 @@ import re
 import string
 import emoji
 from tweet_level import remove_urls
+from nltk.tokenize import sent_tokenize
 
 #to change when tweepy works
 def remove_mention(text):
@@ -14,7 +15,14 @@ def remove_punctuation(text):
         text = text.replace(c,'')
     return text
 
+def remove_emoji(text):
+    for el in text:
+        if emoji.is_emoji(el):
+            text = text.replace(el,'')
+    return text
+
 def clearText(text):
+    text = remove_emoji(text)
     return remove_punctuation(remove_urls(text))
 
 def contains_number(text):
@@ -55,6 +63,7 @@ def avg_word_length(text):
     return sum/len(text)
 
 def text_length(text):
+    """return the length of the text according to the Twitter character counting"""
     count = 0
     text = remove_mention(text)
     for word in text:
@@ -64,6 +73,83 @@ def text_length(text):
             count +=1
     return count
 
-print(text_length("This is a @prova"))
+def is_all_uppercase(text):
+    """check if the text are all in uppercase"""
+    text = text.replace(" ",'')
+    count =0
+    for word in text:
+        if word == word.upper():
+            count +=1
+    return True if count == len(text) else False
+
+def contains_uppercase_text(text):
+    """check if the text contain a sequence of chatacter of length five or larger that contain only uppercase letters"""
+    text = remove_mention(text)
+    text = remove_emoji(text)
+    text = remove_urls(text)
+    text = text.replace(" ",'')
+    cont=False
+    count =0
+    for word in text:
+        if word == word.upper():
+            count +=1
+            if count >= 5 : cont=True
+        else:
+            count = 0
+    return cont
+
+def count_upper_letter(text):
+    """Counts the number of uppercase letter"""
+    text = text.replace(" ",'')
+    count =0
+    for word in text:
+        if word == word.upper():
+            count +=1
+    return count
+
+def ratio_capitalized_words(text):
+    """Counts the number of capitalized words and relates them to the word count.
+    Words with only capitalized characters are not included in the count for capitalized words."""
+    text = remove_mention(text)
+    text = remove_emoji(text)
+    text = remove_urls(text)
+    sum=leng=0
+    #text = text.replace(" ",'')
+    text = text.split()
+    for word in text:
+        leng+=len(word)
+        if word == word.upper():
+            continue
+        else:
+            sum += (count_upper_letter(word))
+    
+    return round(sum/leng,2)
+
+def ratio_all_capitalized_words(text):
+    """Ratio of words that contain only capitalized letters to the total number of words."""
+    text = remove_mention(text)
+    text = remove_emoji(text)
+    text = remove_urls(text)
+    sum=leng=0
+    text=text.split()
+
+    for word in text:
+        leng+=len(word)
+        if word != word.upper():
+            continue
+        else:
+            sum += (count_upper_letter(word))
+    
+    return round(sum/leng,2)
+
+def num_of_sentences(text):
+    """Counts the number of sentences."""
+    tok=sent_tokenize(text)
+    return len(tok)
+
+
+print(no_text("This😂😂 is a @prova 😂"))
 text = """ ciao amico -12  "bla bla bla" mi piace giovanna """
-print(no_text(text), text)
+text = "CIA A TUTTg. Sono bello U.S. Miao"
+
+print(num_of_sentences(text), text)
