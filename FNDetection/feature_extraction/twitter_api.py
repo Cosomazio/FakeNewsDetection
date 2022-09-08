8f5df6798b64008c51447a38411d1fc6ba7a070c
francesco cafiero <f.cafiero1@studenti.unisa.it>
2022-09-08 15:41:51 +0200

    ADD functions to access the api v1 and v2

--- FNDetection/feature_extraction/twitter_api.py
+++ FNDetection/feature_extraction/twitter_api.py
@@ -1,33 +1,27 @@
 import tweepy
 
-bearer_token = ""
-consumer_key = ""
-consumer_secret = ""
-
-access_token = ""
-access_token_secret= ""
-
 """ autenticazione quando viene abilitato l'accesso elevated"""
-#auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
-#auth.set_access_token(access_token,access_token_secret)
-#api = tweepy.API(auth)
-#query = "covid lang:en"
+def api_v1_connection():
+    consumer_key = ""
+    consumer_secret = ""
 
+    access_token = ""
+    access_token_secret= ""
+    auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+    auth.set_access_token(access_token,access_token_secret)
+    api = tweepy.API(auth)
 
-"""accesso per api v2 attraverso client """
-client = tweepy.Client(bearer_token=bearer_token,consumer_key=consumer_key, consumer_secret=consumer_secret, access_token=access_token, access_token_secret=access_token_secret)
-#print(client.get_tweet(id="552791196247269378"))
+    return api
 
-ids=[552791196247269378,529687410611728384]
-tweets= client.get_tweets(ids=ids, expansions=['author_id'])
-print(tweets.data)
-#users = client.get_users(tweets.data['id'])
-#user_fields=["verified,location,public_metrics,profile_image_url,default_profile"]
-for t in tweets.data:
-    print("AAA",t.author_id)
-    print("user: ", client.get_user(id=t.data.author_id,user_fields=["verified,location,public_metrics,profile_image_url"]).data.public_metrics)
-#file = "/Users/cosimocorbisiero/Downloads/train.csv"
+"""accesso per api v2 attraverso client"""
+def api_v2_connection():
+    bearer_token = ""
+    consumer_key = ""
+    consumer_secret = ""
 
-# #b=tweepy.Cursor(api.search_tweets(q="covid")).items(10)
-# for a in tweets.data:
-#     print(a.text)
+    access_token = ""
+    access_token_secret= ""
+    client = tweepy.Client(bearer_token=bearer_token,consumer_key=consumer_key, consumer_secret=consumer_secret, access_token=access_token, access_token_secret=access_token_secret)
+
+    return client
+
