7a7c751c4050edc8e6257b1d5a6d850eb7367f5e
francesco cafiero <f.cafiero1@studenti.unisa.it>
2022-09-09 11:41:41 +0200

    ADD sentiment related features

--- FNDetection/feature_extraction/sentiment_analysis.py
+++ FNDetection/feature_extraction/sentiment_analysis.py
@@ -1,17 +1,72 @@
-from content_level import remove_mention, remove_urls, remove_slang, pos_tag, lemmatize
-from emoji import is_emoji
-from nltk.sentiment import SentimentIntensityAnalyzer
+from text_processing import tokenization, remove_urls, remove_mention, replace_slang, lemmatize
+from nltk import pos_tag
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+def word_score(word):
+    analyzer = SentimentIntensityAnalyzer()
+    pos_score = analyzer.polarity_scores(word)['pos']
+    neg_score = analyzer.polarity_scores(word)['neg']
+    return ((pos_score-neg_score)+1)/2
 
 def sentiment_score(text):
-    analyzer = SentimentIntensityAnalyzer()
     text = remove_mention(remove_urls(text))
-    pos_score = analyzer.polarity_scores(text)['pos']
-    neg_score = analyzer.polarity_scores(text)['neg']
-    #devo tokenizzare le parole e fare len(stringa di parole)
-    parole=text.split()
-    score = (((pos_score - neg_score)/len(parole))+1)/2
+    if(len(text)==0):
+        return 0.5
+    token_text = tokenization(text)
+    token_text = replace_slang(token_text)
+    wordslist = [w.split()[-1] for w in token_text if len(w.split())>0]
+
+    tagged = pos_tag(wordslist)
+    score=0
+    for word, tag in tagged:
+        lemma_word = lemmatize(word, tag[0])
+        
+        score+=word_score(lemma_word)
+    
+    score = (score/len(wordslist))
     return score
 
-text="<3" 
-anal = SentimentIntensityAnalyzer()
-print(sentiment_score(text))
+def nr_pos_sentiment_words(text):
+    text = remove_mention(remove_urls(text))
+    if(len(text)==0):
+        return 0.5
+    token_text = tokenization(text)
+    token_text = replace_slang(token_text)
+    print(token_text)
+    wordslist = [w.split()[-1] for w in token_text if len(w.split())>0]
+
+    tagged = pos_tag(wordslist)
+    count=0
+    for word, tag in tagged:
+        lemma_word = lemmatize(word, tag[0])
+
+        if word_score(lemma_word) >0.5:
+            count +=1
+
+    return count
+
+def nr_neg_sentiment_words(text):
+    text = remove_mention(remove_urls(text))
+    if(len(text)==0):
+        return 0.5
+    token_text = tokenization(text)
+    token_text = replace_slang(token_text)
+    wordslist = [w.split()[-1] for w in token_text if len(w.split())>0]
+
+    tagged = pos_tag(wordslist)
+    count=0
+    for word, tag in tagged:
+        lemma_word = lemmatize(word, tag[0])
+
+        if word_score(lemma_word) < 0.5:
+            count +=1
+
+    return count
+
+
+if __name__ == "__main__":
+    text="this text :) is a try" 
+    anal = SentimentIntensityAnalyzer()
+    print(sentiment_score(text))
+    print(nr_pos_sentiment_words(text))
+    print(nr_neg_sentiment_words(text))
