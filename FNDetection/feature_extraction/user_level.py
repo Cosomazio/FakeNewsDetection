9d215e7f1d998d3153db34765bccfc04eaf21652
francesco cafiero <f.cafiero1@studenti.unisa.it>
2022-09-26 16:52:50 +0200

    functions refactor

--- FNDetection/feature_extraction/user_level.py
+++ FNDetection/feature_extraction/user_level.py
@@ -4,58 +4,46 @@ import time
 import datetime
 from datetime import date
 
-def get_description(user_id, v2_connection):
-    client = v2_connection
-    user = client.get_user(id=user_id,user_fields=["description"])
-    return user.data.description
-
-def has_desc(user_id, v2_connection):
-    if len(get_description(user_id, v2_connection)) != 0:
-        return True
-    return False
-
-def desc_contains_hashtags(user_id, v2_connection):
-    result = nr_of_hashtag(get_description(user_id, v2_connection))
+def desc_contains_hashtags(user):
+    result = nr_of_hashtag(get_description(user))
     if result:
         return True
     return False
 
-def desc_contains_user_mention(user_id, v2_connection):
-    desc = get_description(user_id, v2_connection)
-    result = num_of_usermention(desc)
+def desc_contains_user_mention(user):
+    desc = get_description(user)
+    result = num_of_usermention(desc, v2_connection)
     if result != 0:
         return True
     return False
 
-def desc_contains_url(user_id, v2_connection):
-    result = nr_of_urls(get_description(user_id, v2_connection))
+def desc_contains_url(user):
+    result = nr_of_urls(get_description(user))
     if result != 0:
         return True
     return False
 
-def desc_length(user_id, v2_connection):
-    return len(get_description(user_id, v2_connection))
+def desc_length(user):
+    return len(get_description(user))
 
-def url_lenght(user_id, v2_connection):
-    client =v2_connection
-    user = client.get_user(id=user_id,user_fields=["url"])
-    return len(user.data.url)
+def url_lenght(user):
+    if hasattr(user, "url"):
+        return len(user.url)
+    else: 0
 
-def friends_per_followers(user_id, v2_connection):
-    friend=friends_count(user_id, v2_connection)
-    follow=followers_count(user_id, v2_connection)
+def friends_per_followers(user):
+    friend=friends_count(user)
+    follow=followers_count(user)
     return round(friend/follow,5) if follow != 0 else 0
 
-def is_following_more_than_100(user_id, v2_connection):
-    return True if followers_count(user_id,v2_connection) >= 100 else False
+def is_following_more_than_100(user):
+    return True if followers_count(user) >= 100 else False
 
-def created_days_ago(user_id, v2_connection):
-    client = v2_connection
+def created_days_ago(user):
     date_format= "%Y-%m-%d"
-    user = client.get_user(id=user_id,user_fields=["created_at"])
     date_today=time.strptime(str(date.today()),date_format)
-    date_today = datetime.datetime.fromtimestamp(time.mktime(date_today))
-    date_created=time.strptime(str(user.data.created_at).split()[0],date_format)
+    date_today = datetime.datetime.fromtimestamp(time.mktime(date_today)) 
+    date_created=time.strptime(str(user.created_at).split()[0],date_format)
     date_created = datetime.datetime.fromtimestamp(time.mktime(date_created))
     return(date_today-date_created).days
 
@@ -64,13 +52,18 @@ if __name__ == "__main__":
     #id=1518948046809219076
     #id=38001497
     id=32871086
-
-    print("Has description:",has_desc(id))
-    print("Description contain hashtags:",desc_contains_hashtags(id))
+    v2_connection = api_v2_connection()
+    v1_connection = api_v1_connection()
+    user = v1_connection.get_user(user_id=id)
+    #print("Has description:",has_desc(id))
+    #print(get_description(id, v2_connection))
+    print("created day ago:",created_days_ago(user))
+    
+    """print("Description contain hashtags:",desc_contains_hashtags(id))
     print("Description contain url:",desc_contains_url(id))
     print("Description contain user mention:",desc_contains_user_mention(id))
     print("Description lenght:",desc_length(id))
     print("User URL lenght:",url_lenght(id))
     print("Friend per followers:",friends_per_followers(id))
     print("Following more than 100:",is_following_more_than_100(id))
-    print("created day ago:",created_days_ago(id))
+    print("created day ago:",created_days_ago(id))"""
