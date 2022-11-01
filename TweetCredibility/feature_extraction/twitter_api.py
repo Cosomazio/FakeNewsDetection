bdb330efc6b551f94ab2b746504d29dc1d982f89
Cosomazio <73337360+Cosomazio@users.noreply.github.com>
2022-11-01 09:53:32 +0100

    adding a folder for tweet credibility

---
+++ TweetCredibility/feature_extraction/twitter_api.py
@@ -0,0 +1,148 @@
+import tweepy
+
+""" autenticazione quando viene abilitato l'accesso elevated"""
+def api_v1_connection():
+    
+    return api
+
+"""accesso per api v2 attraverso client"""
+def api_v2_connection():
+    
+
+""" return the author of a tweet, given the tweet id"""
+def get_utente(twid, v2_connection, v1_connection):
+    tweet = v2_connection.get_tweet(twid, expansions=['author_id'])
+    uid = tweet.data.author_id if tweet.data !=None else None
+    return v1_connection.get_user(user_id=uid) if uid !=None else None
+
+def get_description(user):
+    if hasattr(user, "description"):
+        return user.description
+    else: return ""
+
+def get_tweet_status(twid, v1_connection):
+    status = v1_connection.get_status(twid)
+    return status
+    
+def get_tweet_text(tweet):
+    if hasattr(tweet, "text"):
+        return tweet.text
+    return "" 
+
+def followers_count(user):
+    if hasattr(user, "followers_count"):
+        return user.followers_count
+    else: return 0
+
+def friends_count(user):
+    if hasattr(user, "friends_count"):
+        return user.friends_count
+    else: return 0
+
+def statuses_count(user):
+    if hasattr(user, "statuses_count"):
+        return user.statuses_count
+    else: return 0
+
+def verified(user):
+    if hasattr(user, "verified"):
+        return user.verified
+    else: return False
+
+def default_profile(user):
+    if hasattr(user, "default_profile"):
+        return user.default_profile
+    else: return False
+
+def profile_background_tile(user):
+    if hasattr(user, "profile_background_tile"):
+        return user.profile_background_tile
+    else: return False
+
+def profile_use_background_image(user):
+    if hasattr(user, "profile_use_background_image"):
+        return user.profile_use_background_image
+    else: return False
+
+
+def favorite_count(tweet_status):
+    if hasattr(tweet_status, "favorite_count"):
+        return tweet_status.favorite_count
+    else: return 0
+
+def retweeted_count(tweet_status):
+    if hasattr(tweet_status, "retweet_count"):
+        return tweet_status.retweet_count
+    else: return 0
+
+def truncated(tweet_status):
+    if hasattr(tweet_status, "truncated"):
+        return tweet_status.truncated
+    else: return False
+
+def possibly_sensitive(tweet_status):
+    if hasattr(tweet_status, "possibly_sensitive"):
+        return tweet_status.possibly_sensitive
+    else:
+        return False
+
+
+
+if __name__ == "__main__":
+
+    #!-------------------
+    #connection = api_v1_connection()
+    client = tweepy.Client(bearer_token="")
+    query=[]
+    query.append("#covid lang:en")
+    query.append("#war lang:en")
+    query.append("#ballodor lang:en")
+
+    for el in query:
+        tweets = client.search_recent_tweets(query=el, max_results=10)
+
+        with open("miao.txt","a") as file:
+            
+            file.write(str(tweets.data))
+            #print(str(tweets.data))
+        # for tweet in tweets.data:
+        #     print(tweet.text)
+        #     if len(tweet.context_annotations) > 0:
+        #         print(tweet.context_annotations)
+    #!-------------------
+    #*QUESTO PUO SERVIRE SE DOBBIAMO SALVARE IL TWEET APPENA PRESO IN FORMATO JSON
+    # import json
+    # id = 1344776928583159817
+    # connection = api_v2_connection()
+    # v1_connection = api_v1_connection()
+
+    
+    # # user=get_utente(id, connection, v1_connection)
+    # tweet_status = get_tweet_status(id, v1_connection)
+    # with open("ciao.json","a") as f:
+    #     json.dump(tweet_status._json,f)
+        
+    # import multiprocessing as mp
+    # print("Number of processors: ", mp.cpu_count())
+    
+    #_______________________________________
+    #twee = connection.get_tweet(id)
+    #print(tweet_status._json)
+    #print(connection.get_user(id=uid, user_fields=['entities']).data.entities)
+    
+    """print(possibly_sensitive(tweet_status))
+    print(retweeted_count(tweet_status))
+    print(favorite_count(tweet_status))
+    print(truncated(tweet_status))
+
+    print(default_profile(user))
+    print(profile_background_tile(user))
+    print(profile_use_background_image(user))
+
+    #print(get_user_id(id, connection))
+    print(followers_count(user))
+    print(friends_count(user))
+    print(statuses_count(user))
+    print(verified(user))
+
+    print("description", get_description(user))"""
---
+++ TweetCredibility/feature_extraction/user_level.py
@@ -0,0 +1,71 @@
+from twitter_api import *
+from feature_extraction.tweet_level import *
+import time
+import datetime
+from datetime import date
+
+def desc_contains_hashtags(user):
+    result = nr_of_hashtag(get_description(user))
+    if result:
+        return True
+    return False
+
+def desc_contains_user_mention(user, v2_connection):
+    desc = get_description(user)
+    result = num_of_usermention(desc)
+    if result != 0:
+        return True
+    return False
+
+def desc_contains_url(user):
+    result = nr_of_urls(get_description(user))
+    if result != 0:
+        return True
+    return False
+
+def desc_length(user):
+    return len(get_description(user))
+
+def url_lenght(user):
+    if hasattr(user, "url"):
+        if user.url is not None:
+            return len(user.url)
+        else: return 0
+    else: return 0
+
+def friends_per_followers(user):
+    friend=friends_count(user)
+    follow=followers_count(user)
+    return round(friend/follow,5) if follow != 0 else 0
+
+def is_following_more_than_100(user):
+    return True if followers_count(user) >= 100 else False
+
+def created_days_ago(user):
+    date_format= "%Y-%m-%d"
+    date_today=time.strptime(str(date.today()),date_format)
+    date_today = datetime.datetime.fromtimestamp(time.mktime(date_today)) 
+    date_created=time.strptime(str(user.created_at).split()[0],date_format)
+    date_created = datetime.datetime.fromtimestamp(time.mktime(date_created))
+    return(date_today-date_created).days
+
+   
+if __name__ == "__main__":
+    #id=1518948046809219076
+    #id=38001497
+    id=32871086
+    v2_connection = api_v2_connection()
+    v1_connection = api_v1_connection()
+    user = v1_connection.get_user(user_id=id)
+    #print("Has description:",has_desc(id))
+    #print(get_description(id, v2_connection))
+    print("created day ago:",created_days_ago(user))
+    
+    """print("Description contain hashtags:",desc_contains_hashtags(id))
+    print("Description contain url:",desc_contains_url(id))
+    print("Description contain user mention:",desc_contains_user_mention(id))
+    print("Description lenght:",desc_length(id))
+    print("User URL lenght:",url_lenght(id))
+    print("Friend per followers:",friends_per_followers(id))
+    print("Following more than 100:",is_following_more_than_100(id))
+    print("created day ago:",created_days_ago(id))"""
